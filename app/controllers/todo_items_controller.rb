1469ac2055f6969cef91ebc588cc496685a99f61
dh <dimmaaah@live.nl>
2016-10-26 10:50:14 +0200

    added controllers

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,11 +1,15 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
-
+  before_action :set_todo_item,  except: [:create]
   def create
     @todo_item = @todo_list.todo_items.create(todo_items_params)
     redirect_to @todo_list
   end
 
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo Item completed."
+  end
   def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
@@ -17,6 +21,9 @@ class TodoItemsController < ApplicationController
   end
 
   private
+  def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+  end
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
